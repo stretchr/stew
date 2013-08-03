8e7604643f3d0e79c1056ae2beb0806f2f093f4b
Mat Ryer <matryer@mac.com>
2013-08-03 12:32:28 -0600

    made PathSeparator and SignatureSeparator configurable

--- objects/map.go
+++ objects/map.go
@@ -11,9 +11,16 @@ import (
 	"strings"
 )
 
-const (
-	pathSeparator      string = "."
-	signatureSeparator        = "_"
+var (
+	// PathSeparator is the character used to separate the elements
+	// of the keypath.
+	//
+	// For example, `location.address.city`
+	PathSeparator string = "."
+
+	// SignatureSeparator is the character that is used to
+	// separate the Base64 string from the security signature.
+	SignatureSeparator = "_"
 )
 
 // Map is a map[string]interface{} with additional helpful functionality.
